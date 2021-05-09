62860f6086dffad8d2514ee96165b353fc9ea024
Valery Asiryan <dmc5mod@yandex.ru>
2021-05-09 04:09:55 +0300

    Update test.m

--- matlab/test.m
+++ matlab/test.m
@@ -6,7 +6,7 @@
 im_ol=rgb2gray(imread('images/left.png'));
 im_or=rgb2gray(imread('images/right.png'));
 
-%% Disparity filter
+%% Disparity
 win=10;
 max_dis=50;
 weight=5;
@@ -15,7 +15,7 @@ tic;
 im_dm = disparity_estimator(im_ol, im_or, win, max_dis, weight);
 toc;
 
-%% Show result
+%% Result
 figure(1);
 imshow(im_dm);
 title('disparity map');
